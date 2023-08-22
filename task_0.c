7890af79b7d08d43ab13ab409204bb8413beb46c
Geokaswc <geokolade@gmail.com>
2023-08-22 08:41:41 -0700

    modified task 0

--- task_0.c
+++ task_0.c
@@ -5,12 +5,12 @@
   * calls the corresponding function
   * Return: 0
   */
+
 int main(void)
 {
 	mary_print("Character:[%c]\n", 'H');
 	mary_print("String:[%s]\n", "I am a string !");
 	mary_print("Percent:[%%]\n");
-	return (0);
 }
 
 
