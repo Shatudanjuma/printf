c5613502b99de913e2588cd0a6534c69ca15292d
Shatudanjuma <maryaishatudanjuma@gmail.com>
2023-08-22 08:20:12 -0700

    updated main.h

--- main.h
+++ main.h
@@ -3,59 +3,75 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
-#include <limits.h>
+#include <string.h>
+#include <stdlib.h> 
+#include <wchar.h> 
 
 #define INT_MAX   __INT_MAX__
 
+int percentage(char c)
+{
+    return (write(1, &c, 1));
+}
+
+int charac(va_list list_of_args)
+{
+    char c = (char) va_arg(list_of_args, int);
+    return (percentage(c));
+}
+
+int print_string(va_list list_of_args)
+{
+    char *str = va_arg(list_of_args, char *);
+    int count = 0;
+
+    if (str == NULL)
+    {
+        str = "(null)";
+    }
+    while(*str)
+    {
+        count += percentage(*str++);
+    }
+    return (count);
+}
+
 int mary_print(const char *format, ...)
 {
-        int groupWork = 0;
-        va_list list_of_args;
+    va_list list_of_args;
+    int count = 0;
 
-        if (format == NULL)
-                return (-1);
-        va_start(list_of_args, format);
-        while (*format)
+    if (format == NULL)
+    return (-1);
+    va_start(list_of_args, format);
+    while (*format)
+    {
+        if(*format != '%')
         {
-        if (*format != '%')
+            count += percentage(*format++);
+            continue;
+        }
+        format++;
+        if(*format == '%')
         {
-        write(1, format, 1);
-                groupWork++;
+            count += percentage('%');
+        }
+        else if (*format == 'c')
+        {
+            count += charac(list_of_args);
+        }
+        else if (*format == 's')
+        {
+            count += print_string(list_of_args);
         }
         else
         {
-                format++;
-                if (*format == '\0')
-                        break;
-                if (*format == '%')
-                {
-                        write(1, format, 1);
-                        groupWork++;
-                }
-                else if (*format == 'c')
-                {
-                char c = va_arg(list_of_args, int);
-
-                write(1, &c, 1);
-
-                groupWork++;
-                }
-                else if (*format == 's')
-                {
-                char *str = va_arg(list_of_args, char*);
-                int str_len = 0;
-
-                while (str[str_len] != '\0')
-                str_len++;
-                write(1, str, str_len);
-                groupWork += str_len;
-                }
-        }
-        format++;
-        }
-        va_end(list_of_args);
-        return (groupWork);
+            count += percentage('%') + percentage(*format);
+        }format++;
+    }
+    va_end(list_of_args);
+    return (count);
 }
 
-#endif /* MAIN_H */
 
+#endif /* MAIN_H */
